2e89c5f2d26f48a4a934f923f07cc320abff3dfd
cars213 <carlosrendons213@gmail.com>
2025-08-26 17:43:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+/*let input = document.getElementById("amigo");
+console.log(input);*/
+
+
+
+function addFriend(){
+    if(document.getElementById("amigo").value != ''){
+        let input = document.getElementById("amigo").value;
+        amigo.push(input);
+        console.log(amigo);
+        list();
+        wash();
+        console.log(amigo.length);
+    }else{
+        alert('Por favor, inserte un nombre.');
+    }
+
+}
+
+function list(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for(i = 0; i < amigo.length ; i++){
+        let elementadd = document.createElement('li');
+        elementadd.textContent = amigo[i];
+        lista.appendChild(elementadd);
+    }
+
+}
+
+function raffleFriend(){
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    if (amigo.length > 0){
+        let raffle = Math.floor(Math.random()*amigo.length);
+        let resultadd = document.createElement('li');
+        resultadd.textContent = amigo[raffle];
+        resultado.appendChild(resultadd);
+        amigo.splice(raffle,1);
+        console.log(raffle);
+        list();
+    }else{
+        alert('No hay amigos que sortear.');
+    }
+
+}
+
+function wash(){
+    let input = document.getElementById("amigo").value = '';
+}
